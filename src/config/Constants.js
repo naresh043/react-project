2a62dc7b8c015294a1d438be44cac6216e0af99f
NareshSanjeev <naresh732003@gmail.com>
2025-07-28 23:02:53 +0530

    feat: Update BASE_URL to remove trailing slash and maintain local development option

--- src/config/Constants.js
+++ src/config/Constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL="https://elearning-backend-qabt.onrender.com/"
+export const BASE_URL="https://elearning-backend-qabt.onrender.com"
+
+// export const BASE_URL="http://localhost:3000"
+
